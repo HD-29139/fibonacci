637dc5df1e4e299c1be631276ceb06a610c4d08f
HD-29139 <santanajefferson594@gmail.com>
2025-05-06 01:49:02 -0300

    yea, working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use num_bigint::BigUint;
+use num_traits::{One, Zero};
+use std::{mem::replace, time::Instant};
+
+
+fn fibonacci(n: usize) -> BigUint {
+    let mut a = BigUint::zero();
+    let mut b = BigUint::one();
+
+    for _ in 0..n {
+        let temp = a + &b;
+        a = replace(&mut b, temp);
+    }
+
+
+    a
+}
 fn main() {
-    println!("Hello, world!");
+    let n = 1; //just for tests
+    let start = Instant::now();
+    let res = fibonacci(n);
+    let time = start.elapsed();
+
+    println!("Fibonacci({}) = {}", n, res);
+    println!("{:?}", time);
 }
