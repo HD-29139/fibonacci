0ca1ac96ede41359b04555151d7f726e27e56964
HD-29139 <santanajefferson594@gmail.com>
2025-05-06 03:01:57 -0300

    Instant::now()

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use num_bigint::BigUint;
 use num_traits::{One, Zero};
-use std::{fs::File, io::Write, mem::replace};
+use std::{fs::File, io::Write, mem::replace, time::Instant};
 
 fn fibonacci(n: usize) -> BigUint {
     let mut a = BigUint::zero();
@@ -14,15 +14,20 @@ fn fibonacci(n: usize) -> BigUint {
     a
 }
 fn main() {
+    
     let mut line = String::new();
     println!("");
     std::io::stdin().read_line(&mut line).expect("failed to read");
     let line: usize = line.trim().parse().expect("please input a number");
+    let start = Instant::now();
     let res = fibonacci(line);
-    let value = res.to_string();
     println!("Fibonacci({}) = {}", line, res);
     
-
+    
+    let value = res.to_string();
     let mut file = File::create("fibonacci.txt").expect("failed to create");
     file.write_all(value.as_bytes()).expect("failed to write");
+
+    let time = start.elapsed();
+    println!("{:?}", time);
 }
